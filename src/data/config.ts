a003f1043dba5b2fbae219c9e0dbc7e8a3104d04
Oleg Y. Danilkiff <13948753+danilkiff@users.noreply.github.com>
2026-07-10 01:41:18 +0300

    feat: goatcounter analytics at enot.goatcounter.com
    Signed-off-by: Oleg Y. Danilkiff <13948753+danilkiff@users.noreply.github.com>

--- src/data/config.ts
+++ src/data/config.ts
@@ -1,4 +1,4 @@
 // GoatCounter code: analytics posts to https://<code>.goatcounter.com/count.
 // Register the code (free) at https://www.goatcounter.com/ to receive data;
 // set to '' to drop the script entirely. Cookie-less, no consent banner.
-export const GOATCOUNTER = 'enot-theme';
+export const GOATCOUNTER = 'enot';
